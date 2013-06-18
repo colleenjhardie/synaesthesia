b7f8380c3929ef0d66ca0916f5783db70dd6f04f
colleenjhardie <c.hardie@gmail.com>
2013-06-17 21:28:43 -0600

    Basic option page, page icon click listener, options.css placeholder

--- js/background.js
+++ js/background.js
@@ -1,9 +1,14 @@
-// Check to see if a tab URL is that of a GAE console.  If so, call the page action.
+// Check to see if a tab URL is that of a GAE console or the GAE 'My Applictions' summary page.  If so, call the page action.
 function checkUrl(tabId, changeInfo, tab) {
   if (tab.url.indexOf('https://appengine.google.com/') > -1) {
     chrome.pageAction.show(tabId);
-  }
+	}
 };
 
 // Listen for changes to any URL in any tab
 chrome.tabs.onUpdated.addListener(checkUrl);
+
+// Listen for clicks on the page icon
+chrome.pageAction.onClicked.addListener(function(tab) {
+	chrome.tabs.create({"url": "./html/options.html"})
+});
